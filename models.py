cd7a48949d78ba11d2e6c3bb0c4b1fd5178477a1
Ammar Hakim <ammarhakim57@gmail.com>
2024-04-12 20:15:34 -0400

    Updated Sprint + Bug DB Tables

--- models.py
+++ models.py
@@ -25,4 +25,20 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    
+class Sprint(db.Model):
+    __tablename__ = 'sprints'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    author = db.relationship('User', backref='sprints')
+
+class Bug(db.Model):
+    __tablename__ = 'bugs'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    bug_type = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    sprint_id = db.Column(db.Integer, db.ForeignKey('sprints.id'), nullable=False)
+    author = db.relationship('User', backref='bugs')
+    sprint = db.relationship('Sprint', backref='bugs')  
